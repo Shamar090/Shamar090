7cd84b5ee129de97a0ac7978613ad621b7dad331
Shamar090 <166341909+Shamar090@users.noreply.github.com>
2024-10-01 12:12:35 -0400

    Delete java/com/techelevator directory

--- java/com/techelevator/tenmo/exception/InsufficientFundsException.java
+++
@@ -1,9 +0,0 @@
-package com.techelevator.tenmo.exception;
-
-// Custom exception class for situations where there are insufficient funds
-public class InsufficientFundsException extends RuntimeException {
-    // Constructor with a custom error message
-    public InsufficientFundsException(String message) {
-        super(message);
-    }
-}
